12b53d57fac94a2225cbf54842aab850872eacd6
TeeNik <teenik3315@gmail.com>
2021-04-16 04:27:48 +0300

    chunk settings

---
+++ Source/MarchingCubes/Public/Generation/ChunkSettings.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "ChunkSettings.generated.h"
+
+
+USTRUCT(BlueprintType)
+struct MARCHINGCUBES_API FChunkSettings
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 NumOfPoints = 0;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float IsoLevel = 0.5f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float CubeSize = 1.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float NoiseScale = 1.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 FloorLevel = 1;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float AdditionValue = 0.1f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float AdditionRadius = 1.0f;
+};
